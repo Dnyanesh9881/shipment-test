ab6c7d43e08b1e77082a46f348f361f94d4cf581
Dnyanesh9881 <suryawanshidnyaneshwar59@gmail.com>
2025-03-26 15:58:12 +0530

    handle changes

--- web/frontend/components/NavigationBar.jsx
+++ web/frontend/components/NavigationBar.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import HomeIcon from '@mui/icons-material/Home';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import ChatIcon from '@mui/icons-material/Chat';
-import InfoIcon from '@mui/icons-material/Info';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-import GroupIcon from '@mui/icons-material/Group';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
 export function NavigationBar() {
   return (
     <div className='navmenu-section'>
         <ul>
         <li title='Home'>
           <NavLink to="/" className={({isActive}) => isActive ? "active": ""}> 
-            <HomeIcon /> 
+            Home
           </NavLink>
         </li>
-        <li title='Menu'><NavLink to="/menu" className={({isActive}) => isActive ? "active": ""}> <ShoppingCartIcon /> </NavLink></li>
+        <li title='Menu'><NavLink to="/menu" className={({isActive}) => isActive ? "active": ""}> Orders </NavLink></li>
         {/* <li title='Search'><NavLink to="/search" className={({isActive}) => isActive ? "active": ""}> <SearchIcon /> </NavLink></li>
         <li title='Chat'><NavLink to="chat" className={({isActive}) => isActive ? "active": ""}> <ChatIcon /> </NavLink></li>
         <li title='About us'><NavLink to="/about" className={({isActive}) => isActive ? "active": ""}> <InfoIcon /> </NavLink></li>
